201c7e0339cc77e94ab81f004d9e29fb3afaae2b
Tiosphere <loong_22@hotmail.com>
2022-11-23 20:59:34 +0700

    reset

--- app.py
+++ app.py
@@ -12,28 +12,5 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/user/login/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        return "signup page"
-    return redirect("home")
-
-
-@app.route("/user/signup/", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        print(request.form)
-        return "success"
-    return redirect("home")
-
-
-@app.route("/user/forget/", methods=["GET", "POST"])
-def forget():
-    if request.method == "POST":
-        print(request.form)
-        return "success"
-    return redirect("home")
-
-
 if __name__ == "__main__":
     app.run(debug=True)

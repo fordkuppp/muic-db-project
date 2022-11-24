cc9ffa0dad5e90b5412847d2e19b9a9f91441442
fordkuppp <fordpatsakorn@gmail.com>
2022-11-25 01:11:55 +0700

    fix string literal bug

--- novel_reader/user.py
+++ novel_reader/user.py
@@ -7,8 +7,12 @@ import bcrypt
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
+
 @bp.route('/register', methods=["POST"])
-def register():   
+def register(): 
+    db = get_db()
+    cur = db.cursor()
+    
     username = request.form["username"]
     password1 = request.form["password1"]
     password2 = request.form["password2"]
@@ -16,14 +20,18 @@ def register():
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password1.encode("utf-8"), salt)
+    print(hashed_password)
+    if (password1 != password2):
+        return "Passwords do not match!"
     
-    statement = f"INSERT INTO reader_db.user (username, email, password, last_login, role_id) VALUES ('{username}', '{email}', '{hashed_password}', '{time}', 1);"
-
-    db = get_db()
-    cur = db.cursor()
-    cur.execute(statement)
+    add_user = ("INSERT INTO reader_db.user "
+                 "(username, email, password, last_login, role_id) "
+                 "VALUES (%s, %s, %s, %s, 1);")
+    data_user = (username, email, hashed_password, time)
+    
+    cur.execute(add_user, data_user)
     db.commit()
-    
+    cur.close()
     
     return redirect("/")
 

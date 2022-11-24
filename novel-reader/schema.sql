615154a049db61ea0cbf610c63e7d956f88d4cdf
fordkuppp <fordpatsakorn@gmail.com>
2022-11-24 22:05:34 +0700

    make 'username' a unique key

---
+++ novel-reader/schema.sql
@@ -0,0 +1,77 @@
+create table genre
+(
+    id   int          not null
+        primary key,
+    name varchar(255) not null
+);
+
+create table role
+(
+    name varchar(8) not null,
+    id   int        not null
+        primary key
+);
+
+create table status
+(
+    id   int          not null
+        primary key,
+    name varchar(255) not null
+);
+
+create table user
+(
+    id         int auto_increment
+        primary key,
+    username   varchar(255) not null,
+    email      varchar(255) not null,
+    password   char(60)     not null,
+    last_login datetime     null,
+    role_id    int          null,
+    constraint user_pk
+        unique (username),
+    constraint role_id
+        foreign key (role_id) references role (id)
+);
+
+create table novel
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(255) not null,
+    image       varchar(255) null,
+    description text         null,
+    created     datetime     not null,
+    modified    datetime     null,
+    genre_id    int          not null,
+    status_id   int          not null,
+    user_id     int          null,
+    constraint genre_id
+        foreign key (genre_id) references genre (id),
+    constraint status_id
+        foreign key (status_id) references status (id),
+    constraint user_id
+        foreign key (user_id) references user (id)
+);
+
+create table bookmark
+(
+    user_id  int not null,
+    novel_id int not null,
+    constraint bookmark_novel_id
+        foreign key (novel_id) references novel (id),
+    constraint bookmark_user_id
+        foreign key (user_id) references user (id)
+);
+
+create table chapter
+(
+    id       int          not null
+        primary key,
+    name     varchar(255) null,
+    novel_id int          not null,
+    content  text         not null,
+    constraint novel_id
+        foreign key (novel_id) references novel (id)
+);
+

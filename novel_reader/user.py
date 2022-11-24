1a4fbf693b6ec919cacd36ca826719ee26e4015c
fordkuppp <fordpatsakorn@gmail.com>
2022-11-25 01:19:34 +0700

    add exception for duplicate username/password

--- novel_reader/user.py
+++ novel_reader/user.py
@@ -3,6 +3,7 @@ from flask import (
 )
 from novel_reader.db import get_db
 from datetime import datetime
+from mysql.connector import IntegrityError
 import bcrypt
 
 bp = Blueprint('user', __name__, url_prefix='/user')
@@ -10,30 +11,34 @@ bp = Blueprint('user', __name__, url_prefix='/user')
 
 @bp.route('/register', methods=["POST"])
 def register(): 
-    db = get_db()
-    cur = db.cursor()
+    try:
+        db = get_db()
+        cur = db.cursor()
+        
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        email = request.form["email"]
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password1.encode("utf-8"), salt)
+        print(hashed_password)
+        if (password1 != password2):
+            return "Passwords do not match!"
+        
+        add_user = ("INSERT INTO reader_db.user "
+                    "(username, email, password, last_login, role_id) "
+                    "VALUES (%s, %s, %s, %s, 1);")
+        data_user = (username, email, hashed_password, time)
+        
+        cur.execute(add_user, data_user)
+        db.commit()
+        cur.close()
+        
+        return redirect("/")
     
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
-    email = request.form["email"]
-    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password1.encode("utf-8"), salt)
-    print(hashed_password)
-    if (password1 != password2):
-        return "Passwords do not match!"
-    
-    add_user = ("INSERT INTO reader_db.user "
-                 "(username, email, password, last_login, role_id) "
-                 "VALUES (%s, %s, %s, %s, 1);")
-    data_user = (username, email, hashed_password, time)
-    
-    cur.execute(add_user, data_user)
-    db.commit()
-    cur.close()
-    
-    return redirect("/")
+    except IntegrityError:
+        return "Username or password is already in used!"
 
 @bp.route("/user/login/", methods=["POST"])
 def login():

35e8ca3991861e0a9d497d9dd82b54ff41e49dbd
fordkuppp <fordpatsakorn@gmail.com>
2022-11-26 01:08:45 +0700

    remove uesr_id from novel
    it's not needed

--- novel_reader/schema.sql
+++ novel_reader/schema.sql
@@ -19,6 +19,34 @@ create table status
     name varchar(255) not null
 );
 
+create table novel
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(255) not null,
+    image       varchar(255) null,
+    description text         null,
+    created     datetime     not null,
+    modified    datetime     null,
+    genre_id    int          not null,
+    status_id   int          not null,
+    constraint genre_id
+        foreign key (genre_id) references genre (id),
+    constraint status_id
+        foreign key (status_id) references status (id)
+);
+
+create table chapter
+(
+    id       int          not null
+        primary key,
+    name     varchar(255) null,
+    novel_id int          not null,
+    content  text         not null,
+    constraint novel_id
+        foreign key (novel_id) references novel (id)
+);
+
 create table user
 (
     id         int auto_increment
@@ -36,26 +64,6 @@ create table user
         foreign key (role_id) references role (id)
 );
 
-create table novel
-(
-    id          int auto_increment
-        primary key,
-    name        varchar(255) not null,
-    image       varchar(255) null,
-    description text         null,
-    created     datetime     not null,
-    modified    datetime     null,
-    genre_id    int          not null,
-    status_id   int          not null,
-    user_id     int          null,
-    constraint genre_id
-        foreign key (genre_id) references genre (id),
-    constraint status_id
-        foreign key (status_id) references status (id),
-    constraint user_id
-        foreign key (user_id) references user (id)
-);
-
 create table bookmark
 (
     user_id  int not null,
@@ -64,16 +72,5 @@ create table bookmark
         foreign key (novel_id) references novel (id),
     constraint bookmark_user_id
         foreign key (user_id) references user (id)
-);
-
-create table chapter
-(
-    id       int          not null
-        primary key,
-    name     varchar(255) null,
-    novel_id int          not null,
-    content  text         not null,
-    constraint novel_id
-        foreign key (novel_id) references novel (id)
 );
 

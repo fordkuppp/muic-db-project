395d9e6f50f4097982031d6c3176beca50e35ec5
Tiosphere <loong_22@hotmail.com>
2022-12-11 19:15:28 +0700

    fix to fit deployment

--- novel_reader/__init__.py
+++ novel_reader/__init__.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from novel_reader.tags import humantime
+from . import db, user, home, admin, novel
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "secret key ;)"
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret key ;)"
 
-    from . import db, user, home, admin, novel
 
-    db.init_app(app)
-    app.register_blueprint(user.bp)
-    app.register_blueprint(home.bp)
-    app.register_blueprint(admin.bp)
-    app.register_blueprint(novel.bp)
-    app.jinja_env.globals["humantime"] = humantime
+db.init_app(app)
+app.register_blueprint(user.bp)
+app.register_blueprint(home.bp)
+app.register_blueprint(admin.bp)
+app.register_blueprint(novel.bp)
+app.jinja_env.globals["humantime"] = humantime
 
-    return app
+
+if __name__ == "__main__":
+    app.run()
